ab714f94db012e02b3bafc8ca0b035b6d4f64f80
Cesar <cesargb@gmail.com>
2021-12-06 14:23:33 +0100

    chore: method exception change to protected

--- src/ErrorHandler.php
+++ src/ErrorHandler.php
@@ -10,9 +10,6 @@ trait ErrorHandler
 
     /**
      * Call function if roteted catch any Exception.
-     *
-     * @param callable $callable
-     * @return self
      */
     public function catch(callable $callable): self
     {
@@ -21,7 +18,7 @@ trait ErrorHandler
         return $this;
     }
 
-    public function exception(Throwable $exception): self
+    protected function exception(Throwable $exception): self
     {
         if ($this->catchCallable) {
             call_user_func($this->catchCallable, $exception);
