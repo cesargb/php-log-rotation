7444e4ad1773eebfb83cc45da3c03d81ed8f6227
Cesar <cesargb@gmail.com>
2022-01-18 20:41:05 +0100

    test: add to test issue 10

--- tests/Processors/RotativeProcessorTest.php
+++ tests/Processors/RotativeProcessorTest.php
@@ -53,4 +53,38 @@ class RotativeProcessorTest extends TestCase
 
         $this->assertFalse(is_file(self::DIR_WORK."file.log.{$numeralCleaned}.gz"));
     }
+
+    public function testRotationProcessorIssue10()
+    {
+        $maxFiles = 5;
+
+        $rotation = new Rotation();
+
+        $rotation->files(5);
+
+        file_put_contents(self::DIR_WORK.'error.log.1.1', microtime());
+
+        foreach (range(1, $maxFiles + 1) as $n) {
+            $content = microtime();
+
+            file_put_contents(self::DIR_WORK.'error.log.1', $content);
+
+            $rotation
+                ->then(function ($f1, $f2) {
+                    $this->assertEquals($f1, self::DIR_WORK.'error.log.1.1');
+                    $this->assertEquals($f2, self::DIR_WORK.'error.log.1');
+                })
+                ->rotate(self::DIR_WORK.'error.log.1');
+
+            $this->assertEquals($content, file_get_contents(self::DIR_WORK.'error.log.1.1'));
+
+        }
+
+        foreach (range(1, $maxFiles) as $n) {
+            $this->assertFileExists(self::DIR_WORK.'error.log.1.'.$n);
+        }
+
+        $this->assertFalse(is_file(self::DIR_WORK.'error.log.1.'.($maxFiles + 1)));
+    }
+
 }
